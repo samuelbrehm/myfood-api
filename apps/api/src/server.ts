2a2e3c31d438138de1a320b3d04eb84e1b16230c
Samuel Brehm <samuelbrehm@gmail.com>
2019-11-08 15:51:58 -0300

    Created first query and mutation users

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,5 +1,32 @@
+import { resolve } from "path";
 import { GraphQLServer } from "graphql-yoga";
 
-const server = new GraphQLServer({});
+const USERS = [
+  { id: 1, name: "Tony Start", email: "tony@avengers.com" },
+  { id: 2, name: "Peter Parker", email: "peter@avengers.com" }
+];
+
+const typeDefs = resolve(__dirname, "schema.graphql");
+const resolvers = {
+  Query: {
+    users: () => USERS
+  },
+  Mutation: {
+    createUser: (parent, args, ctx, info) => {
+      const { data } = args;
+      const user = {
+        ...data,
+        id: USERS.length + 1
+      };
+      USERS.push(user);
+      return user;
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  resolvers,
+  typeDefs
+});
 
 export default server;

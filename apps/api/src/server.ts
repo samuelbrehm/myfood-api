8456f0a06d395b5348717db5d4176ff478276597
Samuel Brehm <samuelbrehm@gmail.com>
2019-11-08 22:55:43 -0300

    Settings eslint and prettier

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,32 +1,32 @@
-import { resolve } from "path";
-import { GraphQLServer } from "graphql-yoga";
+import { resolve } from 'path'
+import { GraphQLServer } from 'graphql-yoga'
 
 const USERS = [
-  { id: 1, name: "Tony Start", email: "tony@avengers.com" },
-  { id: 2, name: "Peter Parker", email: "peter@avengers.com" }
-];
+  { id: 1, name: 'Tony Start', email: 'tony@avengers.com' },
+  { id: 2, name: 'Peter Parker', email: 'peter@avengers.com' },
+]
 
-const typeDefs = resolve(__dirname, "schema.graphql");
+const typeDefs = resolve(__dirname, 'schema.graphql')
 const resolvers = {
   Query: {
-    users: () => USERS
+    users: (): typeof USERS => USERS,
   },
   Mutation: {
-    createUser: (parent, args, ctx, info) => {
-      const { data } = args;
+    createUser: (parent, args): typeof USERS => {
+      const { data } = args
       const user = {
         ...data,
-        id: USERS.length + 1
-      };
-      USERS.push(user);
-      return user;
-    }
-  }
-};
+        id: USERS.length + 1,
+      }
+      USERS.push(user)
+      return user
+    },
+  },
+}
 
 const server = new GraphQLServer({
   resolvers,
-  typeDefs
-});
+  typeDefs,
+})
 
-export default server;
+export default server
